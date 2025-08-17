30c728fc58639e704469b50ae67288651e4888ab
Manu <manu.saji@gmail.com>
2025-08-17 22:25:29 +0530

    deploying

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from backend.routes import compare, price
+from routes import compare, price
   # Updated import paths
 
 load_dotenv()
