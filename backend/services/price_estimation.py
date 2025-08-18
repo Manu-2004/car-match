fdf75062bb57dbe227d75b9634449c39720e294c
Manu <manu.saji@gmail.com>
2025-08-18 14:31:53 +0530

    wow

--- backend/services/price_estimation.py
+++ backend/services/price_estimation.py
@@ -1,6 +1,6 @@
 import os
 import re
-from langchain_community.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
 from langchain.schema import HumanMessage, SystemMessage
 from dotenv import load_dotenv
